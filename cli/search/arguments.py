bce17499c71e75febc3aff3107ce15a1b53acfed
Zenrich Shadowstep <crypto_code_weaver_syndicate@proton.me>
2026-09-19 12:16:46 -0500

    Match authoritative shell argument parsing

--- cli/search/arguments.py
+++ cli/search/arguments.py
@@ -1,10 +1,24 @@
-import argparse
+from types import SimpleNamespace
+
 FLAGS={'--ALL':'all_aggressive','-A':'all_aggressive','--all':'all','--aliases':'aliases','-a':'aliases','--bin':'bin','--boot':'boot','-B':'boot','--builtins':'builtins','-b':'builtins','--command':'command','-c':'command','--etc':'etc','-E':'etc','--files':'files','-F':'files','--functions':'functions','-f':'functions','--home':'home','-H':'home','--installed':'installed','-i':'installed','--lib':'lib','-L':'lib','--manpages':'manpages','--man':'manpages','-M':'manpages','--modules':'modules','-m':'modules','--not-installed':'not_installed','-n':'not_installed','--process':'process','-x':'process','--packages':'packages','-p':'packages','--pkg':'packages','--path':'path','-P':'path','--sbin':'sbin','--systemd':'systemd','-s':'systemd','--system':'system','-R':'system','--usr':'usr','-U':'usr','--var':'var','-V':'var','--opt':'opt','-O':'opt'}
+MODIFIERS={'--less':'less','--sort':'sort','--pipe':'pipe','--glob':'glob','--excludeDotFiles':'exclude_dotfiles','--nodot':'exclude_dotfiles'}
+
 def parse_arguments(argv):
-    p=argparse.ArgumentParser(add_help=False,allow_abbrev=False)
-    p.add_argument('-h','--help',action='store_true'); p.add_argument('-v','--version',action='store_true')
-    p.add_argument('--less',action='store_true'); p.add_argument('--sort',action='store_true'); p.add_argument('--pipe',action='store_true'); p.add_argument('--glob',action='store_true')
-    p.add_argument('--excludeDotFiles','--nodot',dest='exclude_dotfiles',action='store_true')
-    for flag,dest in FLAGS.items(): p.add_argument(flag,dest=dest,action='store_true')
-    p.add_argument('search_terms',nargs=argparse.REMAINDER)
-    return p.parse_known_args(argv)
+    values={k:False for k in set(FLAGS.values())}
+    values.update({k:False for k in ('help','version','less','sort','pipe','glob','exclude_dotfiles')})
+    terms=[]; stop=False
+    for arg in argv:
+        if stop:
+            terms.append(arg); continue
+        if arg=='--':
+            stop=True; continue
+        if arg in ('-h','--help'):
+            values['help']=True; continue
+        if arg in ('-v','--version'):
+            values['version']=True; continue
+        if arg in FLAGS:
+            values[FLAGS[arg]]=True; continue
+        if arg in MODIFIERS:
+            values[MODIFIERS[arg]]=True; continue
+        terms.append(arg)
+    return SimpleNamespace(**values,search_terms=terms), []
