376341368d091adc516f029a513c865227e207c0
Zenrich Shadowstep <crypto_code_weaver_syndicate@proton.me>
2026-09-20 11:43:58 -0500

    Combine environment and shell variable discovery

--- cli/environment_searches/__init__.py
+++ cli/environment_searches/__init__.py
@@ -1,4 +1,10 @@
-from .environment import search_environment
+from .environment import search_environment as _search_environment
 from .shell_variables import search_shell_variables
+
+
+def search_environment(pattern=""):
+    yield from _search_environment(pattern)
+    yield from search_shell_variables(pattern)
+
 
 __all__ = ["search_environment", "search_shell_variables"]
